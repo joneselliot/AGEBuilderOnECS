ba1dd2efdd1bd42b47c898a7096508681126b19e
joneselliot <80350235+joneselliot@users.noreply.github.com>
2024-01-11 09:24:01 -0800

    update installation source paths to prod

--- globalVariables.ps1
+++ globalVariables.ps1
@@ -1,12 +1,12 @@
 # Contains global variables required for the script to run
 $softwareVersionsPath = "$PSScriptRoot\BuilderVersions.csv"
-# $localinstallersPath = "\\cpde-ess-data\ess-data\static_data\software\IIS_Modules"
-# $dotnetHostingBundleInstaller = "dotnet-hosting-6.0.15-win"
-# $webDeployInstaller = "WebDeploy_amd64_en-US"
+$localinstallersPath = "\\cpde-ess-data\ess-data\static_data\software\IIS_Modules"
+$dotnetHostingBundleInstaller = "dotnet-hosting-6.0.15-win.exe"
+$webDeployInstaller = "WebDeploy_amd64_en-US.msi"
 $tempPath = "C:\temp\AGEBuilderOnECS"
 $arcgisConfigurationUtilityPath = "C:\Program Files\ArcGIS\Server\tools\configurebasedeployment\configurebasedeployment.bat"
 
 #Dev 
-$localinstallersPath = "C:\AGEBuilderOnECS\prereqs"
-$dotnetHostingBundleInstaller = "dotnet-hosting-6.0.25-win.exe"
-$webDeployInstaller = "WebDeploy_amd64_en-US.msi"
+# $localinstallersPath = "C:\AGEBuilderOnECS\prereqs"
+# $dotnetHostingBundleInstaller = "dotnet-hosting-6.0.25-win.exe"
+# $webDeployInstaller = "WebDeploy_amd64_en-US.msi"
